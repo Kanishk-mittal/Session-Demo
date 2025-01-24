db54f1c08caa357a819ac6224ea9b748d4849d36
Kanishk-mittal <kanishkmittal2005@gmail.com>
2025-01-24 12:51:22 +0530

    files for session

---
+++ clutter_manger.py
@@ -0,0 +1,1 @@
+# here we will try to clerar the random folder
---
+++ random_folder/third.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout<<"This is the third file in the random_folder"<<endl;
+    return 0;
+}
